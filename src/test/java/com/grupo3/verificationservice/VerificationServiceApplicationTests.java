51ebc8c04d86c391d4eca9a928e5e4555e4f4af0
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-10 00:47:43 -0600

    🌱 iniciar proyecto de Spring Boot

---
+++ src/test/java/com/grupo3/verificationservice/VerificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.verificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
