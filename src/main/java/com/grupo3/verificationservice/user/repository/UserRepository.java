47b08858e57b1acace4d6ef776ad48ae732c8f9b
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-10 15:02:16 -0600

    Crear interfaz para repositorio de usuarios

---
+++ src/main/java/com/grupo3/verificationservice/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.grupo3.verificationservice.user.repository;
+
+import com.grupo3.verificationservice.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
