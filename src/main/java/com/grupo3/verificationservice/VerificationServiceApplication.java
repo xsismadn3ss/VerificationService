e1ed54b8b3cd7d6bbff7ae2f82bc3d741e69dc1c
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-16 14:49:40 -0600

    ✨  habilitar clientes feign
    Habilitar clientes que usan Feign en el proyecto para poder conectar microservicios

--- src/main/java/com/grupo3/verificationservice/VerificationServiceApplication.java
+++ src/main/java/com/grupo3/verificationservice/VerificationServiceApplication.java
@@ -2,8 +2,10 @@ package com.grupo3.verificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class VerificationServiceApplication {
 
     public static void main(String[] args) {
