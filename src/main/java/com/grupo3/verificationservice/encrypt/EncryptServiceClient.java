fef0d50ad5fb53e154d708e115ad5a47a981122d
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-16 14:51:02 -0600

    👔 Añadir cliente para encriptado
    definir cliente para conectarse al microservicio de encriptado

--- src/main/java/com/grupo3/verificationservice/encrypt/EncryptServiceClient.java
+++ src/main/java/com/grupo3/verificationservice/encrypt/EncryptServiceClient.java
@@ -1,4 +1,13 @@
 package com.grupo3.verificationservice.encrypt;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import shareddtos.usersmodule.auth.EncryptDto;
+import shareddtos.usersmodule.auth.MessageDto;
+
+@FeignClient(name = "encrypt-service", url = "${app.clients.encrypt-service-url}")
 public interface EncryptServiceClient {
+    @PostMapping("/")
+    MessageDto encrypt(@RequestBody EncryptDto encryptDto);
 }
