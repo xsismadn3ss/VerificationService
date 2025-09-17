878483378542344121d95814258ae45d496a3f4e
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-16 21:59:55 -0600

    Definir DTO para confirmar código de verificación

---
+++ src/main/java/com/grupo3/verificationservice/codes/dto/ConfirmCodeDto.java
@@ -0,0 +1,35 @@
+package com.grupo3.verificationservice.codes.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * <h2>Confirm Code DTO</h2>
+ * <p>DTO utilizado para validar código de
+ * confirmación al crear una nueva cuenta</p>
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConfirmCodeDto {
+    /**
+     * Correo electrónico al que fue enviado el código
+     */
+    @NotNull(message = "el campo email no puede ser nulo")
+    @NotBlank(message = "el campo email no debe estar vacío")
+    @Email(message = "el email debe ser valido")
+    private String email;
+
+    /**
+     * Código de verificación ingresado por el usuario
+     */
+    @NotNull(message = "el campo code no puede ser nulo")
+    @NotBlank(message = "el campo code no puede estar vacío")
+    @Length(min = 6, max = 6, message = "el campo code no debe ser de solo 6 dígitos")
+    private String code;
+}
