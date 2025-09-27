5aa80ae9833b43b947c63782b8ee60246fc9942e
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-27 11:28:14 -0600

    añadir tiempo de expiración

--- src/main/java/com/grupo3/verificationservice/codes/service/impl/CodeCacheServiceImpl.java
+++ src/main/java/com/grupo3/verificationservice/codes/service/impl/CodeCacheServiceImpl.java
@@ -4,6 +4,8 @@ import com.grupo3.verificationservice.codes.service.ICodeCacheService;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
 @Service
 public class CodeCacheServiceImpl implements ICodeCacheService {
     private final RedisTemplate<String, Object> redisTemplate;
@@ -14,7 +16,7 @@ public class CodeCacheServiceImpl implements ICodeCacheService {
 
     @Override
     public void saveCode(String email, String code) {
-        redisTemplate.opsForValue().set(email+"_code", code);
+        redisTemplate.opsForValue().set(email+"_code", code, Duration.ofMinutes(5));
     }
 
     @Override
