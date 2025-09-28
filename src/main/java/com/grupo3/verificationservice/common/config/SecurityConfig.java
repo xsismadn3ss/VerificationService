7ffb9877d726159378ad41178a13cdde63c8428d
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-27 23:13:51 -0600

    eliminar match no uttilizado

--- src/main/java/com/grupo3/verificationservice/common/config/SecurityConfig.java
+++ src/main/java/com/grupo3/verificationservice/common/config/SecurityConfig.java
@@ -58,7 +58,6 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers("/encryption/**", "/authentication/**").permitAll()
                         .anyRequest().permitAll()
                 );
         return http.build();
