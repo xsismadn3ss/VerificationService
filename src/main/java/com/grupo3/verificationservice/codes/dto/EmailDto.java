f3a7121612e40e4cd0d3ae9b6fb5989889907728
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-28 01:23:46 -0600

    crear dto para email

---
+++ src/main/java/com/grupo3/verificationservice/codes/dto/EmailDto.java
@@ -0,0 +1,16 @@
+package com.grupo3.verificationservice.codes.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailDto {
+    @NotBlank
+    @Email
+    private String email;
+}
