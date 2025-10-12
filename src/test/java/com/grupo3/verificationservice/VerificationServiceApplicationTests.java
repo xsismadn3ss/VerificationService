1cb16ee65ecaa2a857b94547bc42303ee2f34146
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-10-12 17:59:41 -0600

    eliminar anotación @SpringBootTest en la clase de test para simplificar ejecución

--- src/test/java/com/grupo3/verificationservice/VerificationServiceApplicationTests.java
+++ src/test/java/com/grupo3/verificationservice/VerificationServiceApplicationTests.java
@@ -1,9 +1,7 @@
 package com.grupo3.verificationservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class VerificationServiceApplicationTests {
 
     @Test
