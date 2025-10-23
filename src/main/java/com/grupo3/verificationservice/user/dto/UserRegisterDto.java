2e751f4eaf310804d0b15c21f37ba0e1410e95ba
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-10-23 17:17:28 -0600

    crear DTO para registrar usuario y agregar validaciones para contraseña

---
+++ src/main/java/com/grupo3/verificationservice/user/dto/UserRegisterDto.java
@@ -0,0 +1,35 @@
+package com.grupo3.verificationservice.user.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import shareddtos.usersmodule.auth.SimpleUserDto;
+import shareddtos.usersmodule.auth.UserDto;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegisterDto extends SimpleUserDto {
+    @NotEmpty(message = "el campo password no puede estar vacío")
+    @Length(min = 8, message = "el campo password no puede ser mayor a 8 caracteres")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$",
+            message = "La contraseña debe contener al menos una letra mayúscula, una minúscula y un número"
+    )
+    private String password;
+
+    public SimpleUserDto toSimpleUserDto(){
+        return new SimpleUserDto(
+                this.getId(),
+                this.getUsername(),
+                this.getFirstName(),
+                this.getLastName(),
+                this.getEmail()
+        );
+    }
+}
